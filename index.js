a13d9969b96d11bde0b086dccaeb4e12d8d40d52
Vivek Anand <08vivek08@gmail.com>
2021-05-19 23:55:06 +0530

    working on headless mode on my system, check it on heroku

--- index.js
+++ index.js
@@ -13,7 +13,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
-let quote = '', signed = false, page, browser;
+let quote = '', signed = false, page;
+const userAgent = 'Mozilla/5.0 (X11; Linux x86_64)' +
+    'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36';
+
+let browser;
+(async () => {
+    browser = await puppeteer.launch({ args: ['--no-sandbox'], headless: true });
+    console.log('opened browser');
+})();
+
 const fetchurl = async () => {
     // console.log(api);
     // console.log(Date.now() - api.locations[0].date);
@@ -34,7 +43,7 @@ const fetchurl = async () => {
             if (quote.length <= 50 && signed) {
                 const data = {
                     'email': 'devilemperror94@gmail.com \t vivekanand081199@gmail.com',
-                    'subject': 'Yahoo auotomatic mailer is ready',
+                    'subject': 'Deployment successful',
                     'text': quote + '\n' + quote.length
                 };
                 await mailer(data);
@@ -43,14 +52,10 @@ const fetchurl = async () => {
         .catch(function (err) {
             console.log(err);
         })
-        .finally(() => {
-            setTimeout(() => {
-                fetchurl();
-            }, 8000);
-        });
 }
-
-// fetchurl();
+setInterval(() => {
+    fetchurl();
+}, 30000);
 
 app.get('/', async (req, res) => {
     fetchurl(url)
@@ -61,7 +66,7 @@ app.get('/', async (req, res) => {
 app.post('/signin', async (req, res) => {
     const data = {
         'email': 'devilemperror94@gmail.com',
-        'subject': 'Yahoo auotomatic mailer is ready',
+        'subject': 'Yahoo automatic mailer is ready',
         'text': 'Logged In Succesful\nnow mailer is ready'
     };
     if (signed || req.headers.authorization.split(' ')[1] !== process.env.YAHOO_SECRET_KEY) { return res.status(400).json({ error: 'Admin already logged in' }); }
@@ -71,6 +76,7 @@ app.post('/signin', async (req, res) => {
         // page.waitForNavigation({ waitUntil: 'networkidle2' }),
         page.click('#verify-code-button')
     ])
+        // await page.screenshot({ path: 'login.png' })
         .then(() => {
             password = '';
             res.status(200).json({
@@ -86,7 +92,7 @@ app.post('/signin', async (req, res) => {
             setTimeout(async () => {
                 signed = true;
                 await page.close();
-            }, 30000);
+            }, 20000);
             return await mailer(data);
         });
 });
@@ -101,65 +107,93 @@ app.post('/admin', async (req, res) => {
 });
 
 const signer = async (password) => {
-    browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
-    page = await browser.newPage();
-    await page.goto('https://login.yahoo.com/', { waitUntil: 'networkidle2' });
+    try {
+        console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@opening page")
+        page = await browser.newPage();
 
-    await page.waitForSelector('input[name="username"]', { visible: true });
-    await page.type('input[name="username"]', process.env.YAHOO_USER, { waitUntil: 'networkidle2', delay: 10 });
-    await Promise.all([
-        page.waitForNavigation({ waitUntil: 'networkidle2' }),
-        page.click('#login-signin')
-    ]);
+        await page.setDefaultTimeout(0);
+        await page.setUserAgent(userAgent);
 
-    await page.type('input[name="password"]', password, { waitUntil: 'networkidle2', delay: 10 });
-    await Promise.all([
-        page.waitForNavigation({ waitUntil: 'networkidle2' }),
-        page.click('#login-signin')
-    ]);
+        console.log("############################opening new page");
+        await page.goto('https://login.yahoo.com/', { waitUntil: 'networkidle2' });
 
-    await Promise.all([
-        page.waitForNavigation({ waitUntil: 'networkidle2' }),
-        page.click('button[value="100"]')
-    ]);
+        console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%finding username");
+        await page.waitForSelector('input[name="username"]', { visible: true });
+        await page.type('input[name="username"]', process.env.YAHOO_USER, { waitUntil: 'networkidle2', delay: 100 });
+        await Promise.all([
+            page.waitForNavigation({ waitUntil: 'networkidle2' }),
+            page.click('#login-signin')
+        ]);
+        console.log("############################searching password");
+        await page.type('input[name="password"]', password, { waitUntil: 'networkidle2', delay: 100 });
+        await Promise.all([
+            page.waitForNavigation({ waitUntil: 'networkidle2' }),
+            page.click('#login-signin')
+        ]);
+
+        console.log("****************************sending otp");
+        await Promise.all([
+            page.waitForNavigation({ waitUntil: 'networkidle2' }),
+            page.click('button[value="100"]')
+        ]);
+    }
+    catch (error) {
+        console.log('************************error---', error);
+    }
 }
 
 const mailer = async (data) => {
-    console.log('###########################data', data);
-    const newPage = await browser.newPage();
+    try {
+        console.log('###########################data', data);
+        const newPage = await browser.newPage();
 
-    console.log('&&&&&&&&&&&&&&&&&&&&&&&opened new page');
+        console.log('&&&&&&&&&&&&&&&&&&&&&&&opened new page');
+        await newPage.setDefaultTimeout(0);
+        await newPage.setUserAgent(userAgent);
 
-    await newPage.goto('https://in.mail.yahoo.com/d/compose/', { waitUntil: 'networkidle2' });
+        await newPage.goto('https://in.mail.yahoo.com/d/compose/', { waitUntil: 'networkidle0' });
+        // console.log('Compose button')
+        // await newPage.waitForSelector('a[aria-label="Compose"]', { visible: true });
+        // await Promise.all([
+        //     // newPage.waitForNavigation({ waitUntil: 'networkidle2' }),
+        //     newPage.click('a[aria-label="Compose"]')
+        // ]);
+        // await newPage.screenshot({ path: 'mail.png' });
+        // setTimeout(async () => {
+        //     await newPage.screenshot({ path: 'bcc.png' });
+        // }, 60000);
+        console.log('@@@@@@@@@@@@@@@@@@@@@@searching bcc button');
+        await newPage.waitForSelector('button[data-test-id="btn-cc"]');
+        console.log('*****************found bcc');
+        await Promise.all([
+            newPage.click('button[data-test-id="btn-cc"]')
+        ]);
+        console.log('#################bcc clicked');
+        await newPage.waitForSelector('#message-bcc-field', { visible: true });
 
-    // await newPage.waitForSelector('a[aria-label="Compose"]', { visible: true });
-    // await Promise.all([
-    //     // newPage.waitForNavigation({ waitUntil: 'networkidle2' }),
-    //     newPage.click('a[aria-label="Compose"]')
-    // ]);
-    await newPage.waitForTimeout(10000);
-    console.log('@@@@@@@@@@@@@@@@@@@@@@searching bcc button');
-    await newPage.waitForSelector('button[data-test-id="btn-cc"]', { visible: true });
-    console.log('*****************found bcc');
-    await Promise.all([
-        newPage.click('button[data-test-id="btn-cc"]')
-    ]);
-    console.log('#################bcc clicked');
+        console.log('**************************Email address');
+        await newPage.type('#message-bcc-field', data.email, { waitUntil: 'networkidle2', delay: 100 });
 
-    await newPage.waitForSelector('#message-bcc-field', { visible: true });
-    await newPage.type('#message-bcc-field', data.email, { waitUntil: 'networkidle2', delay: 10 });
+        console.log('------------Subject');
+        await newPage.waitForSelector('input[aria-label="Subject"]', { visible: true });
+        await newPage.type('input[aria-label="Subject"]', data.subject, { waitUntil: 'networkidle2', delay: 100 });
+        console.log('------------Message');
 
-    // await newPage.waitForSelector('input[aria-label="Subject"]', { visible: true });
-    await newPage.type('input[aria-label="Subject"]', data.subject, { waitUntil: 'networkidle2', delay: 10 });
+        await newPage.waitForSelector('div[aria-label="Message body"]', { visible: true });
+        await newPage.type('div[aria-label="Message body"]', data.text, { waitUntil: 'networkidle2', delay: 100 });
 
-    await newPage.type('div[aria-label="Message body"]', data.text, { waitUntil: 'networkidle2', delay: 10 });
+        await newPage.waitForSelector('button[title="Send this email"]', { visible: true });
+        await Promise.all([
+            newPage.waitForNavigation({ waitUntil: 'networkidle2' }),
+            newPage.click('button[title="Send this email"]')
+        ]);
+        console.log('------------email sent');
 
-    await newPage.waitForSelector('button[title="Send this email"]', { visible: true });
-    await Promise.all([
-        // newPage.waitForNavigation({ waitUntil: 'networkidle2' }),
-        newPage.click('button[title="Send this email"]')
-    ]);
-    return await newPage.close();
+        await newPage.close();
+    }
+    catch (error) {
+        console.log('########################################ERROR:---------', error);
+    }
 }
 
 app.listen(process.env.PORT, () => {
